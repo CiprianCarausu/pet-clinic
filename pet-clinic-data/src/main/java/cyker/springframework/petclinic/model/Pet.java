1a04bc920404bd5c15aa59ef99e759befe83aae2
Ciprian Carausu <carausuciprian@gmail.com>
2020-10-24 17:05:26 +0300

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/cyker/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/cyker/springframework/petclinic/model/Pet.java
@@ -2,7 +2,8 @@ package cyker.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
