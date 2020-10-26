47441a16cb211eb27fb05ab3b3efc5b0993f812d
Ciprian Carausu <carausuciprian@gmail.com>
2020-10-26 15:57:16 +0200

    Added Specialties Map Impl. Closes #46

---
+++ pet-clinic-data/src/main/java/cyker/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package cyker.springframework.petclinic.services;
+
+import cyker.springframework.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
