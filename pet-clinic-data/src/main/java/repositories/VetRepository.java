619a92e5b8791385921fcb03dcf970d041254672
Ciprian Carausu <carausuciprian@gmail.com>
2020-10-27 13:13:22 +0200

    added PetType SD JPA Service. Closes #43

--- pet-clinic-data/src/main/java/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/repositories/VetRepository.java
@@ -3,5 +3,6 @@ package repositories;
 import cyker.springframework.petclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface VetRepository extends CrudRepository<Vet, Long> {
 }
